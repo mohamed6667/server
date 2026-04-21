76ccc0b9293429f6bd9fbf7d35aa33122eb5cac7
Mohamed <mohamedshekshok9@gmail.com>
2026-04-21 04:24:50 +0200

    telegram working

--- server.js
+++ server.js
@@ -1,25 +1,39 @@
 const express = require("express");
 const cors = require("cors");
-const fs = require("fs");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.post("/register", (req, res) => {
+const TOKEN = "8701918863:AAEPTmID2U-Z2ZGj-pUthC0RH6__VdyYbYI";
+const CHAT_ID = "1879456094";
+
+app.post("/register", async (req, res) => {
   const phone = req.body.phone;
 
-  console.log("رقم جديد:", phone);
+  const message = `📱 رقم جديد:\n${phone}`;
 
-  // حفظ في ملف
-  fs.appendFileSync("numbers.txt", phone + "\n");
+  try {
+    await fetch(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        chat_id: CHAT_ID,
+        text: message
+      })
+    });
 
-  res.json({
-    message: "تم التسجيل بنجاح ✅"
-  });
+    res.json({ message: "تم التسجيل بنجاح ✅" });
+
+  } catch (error) {
+    res.json({ message: "حصل خطأ ❌" });
+  }
 });
 
-app.listen(3000, () => {
-  console.log("Server شغال على http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running");
 });
